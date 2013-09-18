ef40921d1af23d20ced1a88f71a0b0aa32f43a26
Paul Tondeur <paul@paultondeur.nl>
2013-09-18 23:16:56 +0200

    improved encapsulation

--- public/js/backbone-command/Command.js
+++ public/js/backbone-command/Command.js
@@ -1,10 +1,12 @@
-Backbone.Command = function () {};
+(function() {
+	Backbone.Command = function () {};
 
-_.extend(Backbone.Command.prototype, {
-	execute:function () {
+	_.extend(Backbone.Command.prototype, {
+		execute:function () {
 
-	}
-});
+		}
+	});
 
-//Like all Backbone classes, make use of the same extend method
-Backbone.Command.extend = Backbone.Router.extend;
+	//Like all Backbone classes, make use of the same extend method
+	Backbone.Command.extend = Backbone.Router.extend;
+})();
--- public/js/backbone-command/CommandRouter.js
+++ public/js/backbone-command/CommandRouter.js
@@ -1,12 +1,14 @@
-Backbone.CommandRouter = Backbone.Router.extend({
+(function() {
+	Backbone.CommandRouter = Backbone.Router.extend({
 
-	bindCommand: function(listenObject, event, command) {
-		listenObject.on(event, this._executeCommand, {command:command});
-	},
+		bindCommand: function(listenObject, event, command) {
+			listenObject.on(event, this._executeCommand, {command:command});
+		},
 
-	_executeCommand: function(){
-		var theCommand = new this.command(this.options);
-		theCommand.execute();
-		theCommand = null;
-	}
-});
+		_executeCommand: function(){
+			var theCommand = new this.command(this.options);
+			theCommand.execute();
+			theCommand = null;
+		}
+	});
+})();
